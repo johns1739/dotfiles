bf0a484791de557b8d72508793de29ddeca34427
Juan Banda Jr <juan@carerev.com>
2023-05-05 12:28:43 -0700

    Fix ripgrep out of memory issue

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -141,7 +141,10 @@ return require('packer').startup(function(use)
             vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Find files in git' })
             vim.keymap.set('n', '<leader><leader>', builtin.find_files, { desc = 'Find files' })
             vim.keymap.set('n', '<leader>bb', builtin.buffers, { desc = 'Find buffers' })
-            vim.keymap.set('n', '<leader>ss', builtin.live_grep, { desc = 'Live search' })
+            vim.keymap.set('n', '<leader>ss',
+                function()
+                    builtin.live_grep({ additional_args = { '-j1' } })
+                end, { desc = 'Live search' })
             vim.keymap.set('n', '<leader>ff', '<cmd>Telescope file_browser path=%:p:h select_buffer=true<CR>',
                 { desc = 'Directory listing' })
             vim.keymap.set('n', '<leader>pp', '<cmd>Telescope project<CR>', { desc = 'Projects' })
