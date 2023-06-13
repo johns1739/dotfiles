0db6107e70438eeb1e433c19c45b2915baa75042
Juan Banda Jr <juan@carerev.com>
2023-06-13 10:14:17 -0700

    init.el

---
+++ emacs/init.el
@@ -0,0 +1,91 @@
+(setq gc-cons-threshold 100000000)
+(add-hook 'after-init-hook
+	  #'(lambda ()
+                               ;; restore after startup
+                               (setq gc-cons-threshold 800000)))
+(setq read-process-output-max (* 1024 1024)) ;; 1mb
+(setq inhibit-startup-message t)
+
+(if (fboundp 'menu-bar-mode)
+    (menu-bar-mode -1))
+(if (fboundp 'tool-bar-mode)
+    (tool-bar-mode -1))
+(if (fboundp 'scroll-bar-mode)
+    (scroll-bar-mode -1))
+
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(unless package--initialized (package-initialize))
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(eval-when-compile
+  (require 'use-package))
+(setq use-package-always-ensure t)
+
+
+;;; Useful Defaults
+(setq-default cursor-type 'bar)           ; Line-style cursor similar to other text editors
+(setq inhibit-startup-screen t)           ; Disable startup screen
+(setq initial-scratch-message "")         ; Make *scratch* buffer blank
+(setq-default frame-title-format '("%b")) ; Make window title the buffer name
+(setq ring-bell-function 'ignore)         ; Disable bell sound
+(fset 'yes-or-no-p 'y-or-n-p)             ; y-or-n-p makes answering questions faster
+(show-paren-mode 1)                       ; Show closing parens by default
+(setq linum-format "%4d ")                ; Line number format
+(delete-selection-mode 1)                 ; Selected text will be overwritten when you start typing
+(global-auto-revert-mode t)               ; Auto-update buffer if file has changed on disk
+(use-package undo-tree                    ; Enable undo-tree, sane undo/redo behavior
+  :init (global-undo-tree-mode)
+  :config (setq-default undo-tree-auto-save-history nil))
+(add-hook 'before-save-hook
+	  'delete-trailing-whitespace)    ; Delete trailing whitespace on save
+(add-hook 'prog-mode-hook                 ; Show line numbers in programming modes
+          (if (and (fboundp 'display-line-numbers-mode) (display-graphic-p))
+              #'display-line-numbers-mode
+            #'linum-mode))
+
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(unless (file-exists-p custom-file)
+  (write-region "" nil custom-file))
+;;; Load custom file. Don't hide errors. Hide success message
+(load custom-file nil t)
+
+;;; Put Emacs auto-save and backup files to /tmp/ or C:/Temp/
+(defconst emacs-tmp-dir (expand-file-name (format "emacs%d" (user-uid)) temporary-file-directory))
+(setq
+   backup-by-copying t                                        ; Avoid symlinks
+   delete-old-versions t
+   kept-new-versions 6
+   kept-old-versions 2
+   version-control t
+   auto-save-list-file-prefix emacs-tmp-dir
+   auto-save-file-name-transforms `((".*" ,emacs-tmp-dir t))  ; Change autosave dir to tmp
+   backup-directory-alist `((".*" . ,emacs-tmp-dir)))
+
+;;; Lockfiles unfortunately cause more pain than benefit
+(setq create-lockfiles nil)
+
+(defun reload-config ()
+  (interactive)
+  (load-file (expand-file-name "init.el" user-emacs-directory)))
+
+(use-package evil
+  :ensure t
+  :init
+  (setq evil-want-integration t) ;; This is optional since it's already set to t by default.
+  (setq evil-want-keybinding nil)
+  :config
+  (evil-mode 1))
+
+(use-package evil-collection
+  :after evil
+  :ensure t
+  :config
+  (evil-collection-init))
+
+(use-package projectile
+  :config
+  (projectile-mode t))
