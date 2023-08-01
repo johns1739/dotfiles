3c82f096cff340c3a93b7f6cecc3c487251c7e20
Juan Banda Jr <juan@carerev.com>
2023-07-31 21:14:12 -0700

    Added scrolloff

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -16,6 +16,7 @@ vim.opt.mouse = "a"
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.ruler = false
+vim.opt.scrolloff = 8
 vim.opt.shiftwidth = 4
 vim.opt.signcolumn = "yes"
 vim.opt.smartcase = true
