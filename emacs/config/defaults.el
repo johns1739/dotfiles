6e61952eda073945532b2b1ba389d540224b2302
Juan Banda Jr <juan@carerev.com>
2024-03-12 02:45:42 -0700

    Dont use hl line for all modes

--- emacs/config/defaults.el
+++ emacs/config/defaults.el
@@ -55,9 +55,9 @@
 
 (add-hook 'prog-mode-hook #'display-line-numbers-mode)
 (add-hook 'prog-mode-hook #'display-fill-column-indicator-mode)
+(add-hook 'prog-mode-hook #'hl-line-mode)
 
 (column-number-mode 1)
-(global-hl-line-mode t)
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
