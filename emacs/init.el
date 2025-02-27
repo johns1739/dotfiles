dd70a84738ab76c7785fc33d09dac6a1d0fdb4fc
Juan <jubajr17@gmail.com>
2025-02-27 13:06:41 -0800

    Created mac & linux ghostty configs

--- emacs/init.el
+++ emacs/init.el
@@ -198,7 +198,8 @@
 (setq completions-detailed t)
 (setq completions-format 'one-column)
 (setq completions-max-height 20)
-(global-completion-preview-mode -1)
+(unless (version< emacs-version "30.0")
+  (global-completion-preview-mode -1))
 
 ;; tab settings
 (setq tab-always-indent t)
