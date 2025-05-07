3ad5784e44a54ecfbebe2f93715284a00cd2c295
Juan Banda <jbanda@digitalrealty.com>
2025-05-07 13:35:44 -0700

    auto-start pinentry

--- config/emacs/packages.el
+++ config/emacs/packages.el
@@ -725,7 +725,10 @@
   (org-capture-templates
    `(("t" "Task" entry (file+headline "tasks.org" "Personal") "* TODO %?" :prepend t :empty-lines 1))))
 
-(use-package pinentry)
+(use-package pinentry
+  :init
+  (with-eval-after-load 'magit
+    (pinentry-start)))
 
 (use-package popper
   :defer 3
