0d6f7449ffce3d6d3e99fa51990d8aafceb3f4ac
Juan Banda Jr <jubajr17@gmail.com>
2025-12-29 23:20:46 -0800

    Required ssh vars for grahpic eamcs

--- config/emacs/packages/setup.el
+++ config/emacs/packages/setup.el
@@ -33,4 +33,6 @@
   (exec-path-from-shell-debug t)
   (exec-path-from-shell-warn-duration-millis 1000)
   :config
+  (dolist (var '("SSH_AUTH_SOCK" "SSH_AGENT_PID" "GPG_AGENT_INFO"))
+    (add-to-list 'exec-path-from-shell-variables var))
   (exec-path-from-shell-initialize))
