bdeb92bf232d2b561bc742c7dc4ad49fb3695e06
Juan Banda <juan.banda@syllo.ai>
2026-06-24 23:14:31 -0700

    refactor(emacs): Update package archive priorities and settings
    - Add nongnu archive, prioritize melpa over melpa-stable Enable
    - native compilation of packages at install time Remove
    - use-package-always-defer setting

--- config/emacs/lisp/emacs-manager.el
+++ config/emacs/lisp/emacs-manager.el
@@ -2,13 +2,18 @@
 
 (setopt package-archives
         '(("gnu" . "https://elpa.gnu.org/packages/")
-          ("melpa-stable" . "https://stable.melpa.org/packages/")
-          ("melpa" . "https://melpa.org/packages/")))
+          ("nongnu" . "https://elpa.nongnu.org/nongnu/")
+          ("melpa" . "https://melpa.org/packages/")
+          ("melpa-stable" . "https://stable.melpa.org/packages/")))
 
 (setopt package-archive-priorities
-        '(("gnu" . 100) ("melpa-stable" . 50) ("melpa" . 1)))
+        '(("gnu" . 100) ("nongnu" . 80) ("melpa" . 50) ("melpa-stable" . 1)))
 
+;; Native-compile packages at install time (requires native-comp support).
+(setopt package-native-compile t)
+
+;; Requires :ensure nil on all built-in packages.
 (setopt use-package-always-ensure t)
-(setopt use-package-always-defer nil) ;; better to pick which packages to defer
 
 (provide 'emacs-manager)
+;;; emacs-manager.el ends here
